bc6a498bf7b546494876998135b30c7d7ad6a2ce
B. van Wetten <bas@vanwetten.com>
2022-01-29 19:08:22 +0100

    Additions
    - Added clang-tidy option (not working with PCH yet)
    - Added clang-format specification

--- anvil/src/anvil/core/Application.h
+++ anvil/src/anvil/core/Application.h
@@ -1,23 +1,23 @@
 #pragma once
 
-namespace Anvil
-{
-    class Application
-    {
-    public:
-      Application(std::string_view name = "Anvil application");
+namespace Anvil {
 
-      void Run();
-      void Close();
-      Application* Get() const { return s_Instance; }
+  class Application {
+  public:
+    Application(std::string_view name = "Anvil application");
 
-    private:
-      virtual ~Application() = default;
+    void         Run();
+    void         Close();
+    Application* Get() const { return s_Instance; }
 
-    private:
-      // TODO: Should we use a smart pointer?
-      static Application* s_Instance;
+  private:
+    virtual ~Application() = default;
 
-      std::string_view m_Name;
-    };
-}
+  private:
+    // TODO: Should we use a smart pointer?
+    static Application* s_Instance;
+
+    std::string_view m_Name;
+  };
+
+} // namespace Anvil
